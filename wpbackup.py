4415aedbc35463ad1dc5a763fe0cc579c771d819
Sulim993 <91571028+Sulim993@users.noreply.github.com>
2022-02-20 16:34:06 +0100

    Update wpbackup.py

--- wpbackup.py
+++ wpbackup.py
@@ -3,36 +3,38 @@ import optparse
 import os
 import datetime
 import shutil
+import socket
 from subprocess import Popen, PIPE
+
  
 date = datetime.datetime.now().strftime('%Y%m%d-%s')
 f_date = datetime.datetime.now().strftime('%Y%m%d')
  
-def backup_all_databases():  # backup all mysql databases
+def backup_all_databases():
     args = ['mysqldump', '-u', 'root', '-p123', '--all-databases']
-    with open("%s.sql.zip" % f_date, 'wb') as f:
+    with open("%s.sql.gz" % f_date, 'wb') as f:
         p1 = Popen(args, stdout=PIPE)
         p2 = Popen('gzip', stdin=p1.stdout, stdout=f)
         p1.stdout.close()
         p2.wait()
         p1.wait()
  
-def html_folder(): # backup wordpress folder
+def tar_html_folder():
     output_filename_1 = "%s.html_dir"  % f_date
     output_filename_2 = "%s.html_dir.zip"  % f_date
     dir_name = '/var/www/html'
-    dst = "%s" % date
+    dst = "%s" % date + socket.gethostname()
     shutil.make_archive(output_filename_1, 'zip', dir_name)
     shutil.move(output_filename_2, dst)
  
-def main():  # create a dump folder to put backup files
-    archive_path = "%s" % date
+def main():
+    archive_path = "%s" % date + socket.gethostname()
     os.mkdir(archive_path, 0755)
     backup_all_databases()
-    src_file = "%s.sql.zip" % f_date
-    dst = "%s" % date
+    src_file = "%s.sql.gz" % f_date
+    dst = "%s" % date + socket.gethostname()
     shutil.move(src_file, dst)
-    html_folder()
+    tar_html_folder()
  
 if __name__ == "__main__":
     main()
